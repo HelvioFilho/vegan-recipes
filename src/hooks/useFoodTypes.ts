c915dcc78f957c54a719b3dc9406b87538024dff
Helvio Filho <helviosvf@gmail.com>
2025-03-07 09:30:41 -0300

    feat: update useFoodTypes to fetch from local database
    Changed useFoodTypes hook to query local SQLite data via getLocalFoodTypes instead of fetching from the internet. This update improves performance and enables offline support for food types data.

--- src/hooks/useFoodTypes.ts
+++ src/hooks/useFoodTypes.ts
@@ -1,17 +1,12 @@
+import { FoodType, getLocalFoodTypes } from '@/services/foodTypesLocal';
 import { useQuery } from '@tanstack/react-query';
-import { api } from '@/services/api';
-
-export type FoodType = {
-  id: number;
-  name: string;
-};
 
 export function useFoodTypes() {
   return useQuery<FoodType[], Error>({
-    queryKey: ['foodTypes'],
+    queryKey: ['localFoodTypes'],
     queryFn: async () => {
-      const response = await api.get('/foodtypes');
-      return response.data;
+      const foods = await getLocalFoodTypes();
+      return foods;
     },
   });
 }
