c27c9c829a452961120261f59316a26dce45e5d4
Helvio Filho <helviosvf@gmail.com>
2025-03-18 19:24:58 -0300

    test(ErrorCard): update tests for refresh functionality
    - Added a new test case to verify that the refresh button calls the handleRefresh prop when pressed.
    - Adjusted render calls in ErrorCard tests to include a dummy handleRefresh function.
    - Verified that the error message, email link, and refresh button are rendered correctly and function as expected.

--- src/__tests__/components/ErrorCard.test.tsx
+++ src/__tests__/components/ErrorCard.test.tsx
@@ -1,28 +1,40 @@
-import { render, fireEvent } from '@testing-library/react-native';
 import { Linking } from 'react-native';
+import { render, fireEvent } from '@testing-library/react-native';
 import { ErrorCard } from '@/components/ErrorCard';
 
 describe('ErrorCard', () => {
   it('should render error message and email correctly', () => {
-    const { getByText } = render(<ErrorCard />);
+    const dummyRefresh = jest.fn();
+    const { getByText } = render(<ErrorCard handleRefresh={dummyRefresh} />);
 
     expect(getByText('Ocorreu um erro inesperado')).toBeTruthy();
     expect(getByText('suporte@vegan-recipes.hsvf.com.br')).toBeTruthy();
     expect(getByText('4010B')).toBeTruthy();
+    expect(getByText('Tentar novamente')).toBeTruthy();
   });
 
   it('should open email client when email link is pressed', () => {
+    const dummyRefresh = jest.fn();
     const openURLSpy = jest
       .spyOn(Linking, 'openURL')
       .mockImplementation(() => Promise.resolve(true));
 
-    const { getByText } = render(<ErrorCard />);
-
+    const { getByText } = render(<ErrorCard handleRefresh={dummyRefresh} />);
     const emailLink = getByText('suporte@vegan-recipes.hsvf.com.br');
     fireEvent.press(emailLink);
 
     expect(openURLSpy).toHaveBeenCalledWith(
       'mailto:suporte@vegan-recipes.hsvf.com.br?subject=Erro%204010B'
     );
+    openURLSpy.mockRestore();
+  });
+
+  it('should call handleRefresh when the refresh button is pressed', () => {
+    const dummyRefresh = jest.fn();
+    const { getByText } = render(<ErrorCard handleRefresh={dummyRefresh} />);
+    const refreshButton = getByText('Tentar novamente');
+    fireEvent.press(refreshButton);
+
+    expect(dummyRefresh).toHaveBeenCalledTimes(1);
   });
 });
